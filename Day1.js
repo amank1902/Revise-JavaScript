d16857ff0f3f5c906592bf09681427cae83845ee
Aman <amankumar190203@gmail.com>
2024-07-25 00:01:14 +0530

    day 1

---
+++ Day1.js
@@ -0,0 +1,25 @@
+var num = 5;
+console.log(num);
+let str = "Ak";
+console.log(str);
+const bl = true;
+console.log(bl);
+
+var person = {
+    name : "joker"
+}
+var arr = [5,10];
+
+console.log(typeof(num));
+console.log(typeof(str));
+console.log(typeof(bl));
+console.log(typeof(person));
+console.log(typeof(arr));
+
+let val = 5;
+console.log(val);
+val = 10;
+console.log(val);
+
+const check = 4;
+check = 8;
